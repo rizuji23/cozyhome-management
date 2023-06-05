cf5cc6b9179fc5b44e50d669d0021f95a4c2038f
rizuji23 <mypcfauzi2@gmail.com>
2023-06-05 12:12:10 +0700

    added zip build

--- src/module/axios.ts
+++ src/module/axios.ts
@@ -4,5 +4,5 @@ const URL_DEV = 'http://localhost:8000';
 const URL_PRODUCTION = 'http://cozyhome.api.weworks.ink'
 
 export default axios.create({
-    baseURL: URL_DEV
+    baseURL: URL_PRODUCTION
 });
