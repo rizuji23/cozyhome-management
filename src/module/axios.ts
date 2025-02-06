b7d88d18fe296f45df1d0b30cc162383689d11d6
rizuji23 <mypcfauzi2@gmail.com>
2025-02-06 11:07:54 +0700

    Change url production

--- src/module/axios.ts
+++ src/module/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const URL_DEV = 'http://localhost:8000';
-export const URL_PRODUCTION = 'https://apicozy.rlstudio.my.id'
+export const URL_DEV = "https://apicozy.rlstudio.my.id";
+export const URL_PRODUCTION = "https://apicozy.rlstudio.my.id";
 
 export default axios.create({
-    baseURL: URL_DEV
+  baseURL: URL_DEV,
 });
